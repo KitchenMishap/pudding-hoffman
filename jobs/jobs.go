d41dd655d8d97b90b2cee0c8f495a2d06037df22
KitchenMishap <hello@kitchenmishap.art>
2026-01-20 05:37:04 +0000

    feat: build the block-height to txo-index array

--- jobs/jobs.go
+++ jobs/jobs.go
@@ -1,6 +1,7 @@
 package jobs
 
 import (
+	"errors"
 	"github.com/KitchenMishap/pudding-huffman/blockchain"
 )
 
@@ -11,10 +12,52 @@ func GatherStatistics(folder string) error {
 		return err
 	}
 	println("Finished opening files.")
-	latestBlock, err := reader.Blockchain().LatestBlock()
+	blockchainInterface := reader.Blockchain()
+	latestBlock, err := blockchainInterface.LatestBlock()
 	if err != nil {
 		return err
 	}
 	println("The last block height is:", latestBlock.Height())
+
+	blocks := latestBlock.Height() + 1
+	blockToTxo := make([]int64, blocks)
+
+	println("Gathering txo indices for each block...")
+	blockHeight := int64(0)
+	blockHandle := blockchainInterface.GenesisBlock()
+	for {
+		if blockHeight%100000 == 0 {
+			println("Block: ", blockHeight)
+		}
+
+		block, err := blockchainInterface.BlockInterface(blockHandle)
+		if err != nil {
+			return err
+		}
+		transHandle, err := block.NthTransaction(0)
+		if err != nil {
+			return err
+		}
+		trans, err := blockchainInterface.TransInterface(transHandle)
+		if err != nil {
+			return err
+		}
+		txoHandle, err := trans.NthTxo(0)
+		if err != nil {
+			return err
+		}
+		if !txoHandle.TxoHeightSpecified() {
+			return errors.New("txo height not specified by handle")
+		}
+		blockToTxo[blockHeight] = txoHandle.TxoHeight()
+
+		blockHeight++
+		if blockHeight == blocks {
+			break
+		}
+		blockHandle, err = blockchainInterface.NextBlock(blockHandle)
+	}
+	println("There are: ", blockToTxo[blocks-1], " txos in the first: ", blocks, " blocks.")
+
 	return nil
 }
